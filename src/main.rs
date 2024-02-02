d32ef86ed9e6b507fea15ebc30763d7abb713210
mkflan <makani.flan@gmail.com>
2024-02-02 16:51:35 -0600

    starting chip-8 assembler

--- src/main.rs
+++ src/main.rs
@@ -4,28 +4,46 @@ mod cpu;
 mod display;
 mod keyboard;
 
-use clap::Parser;
+use clap::{Parser, Subcommand};
 use cpu::Cpu;
 use display::{Display, HEIGHT, SCALE, WIDTH};
 use std::{error::Error, fs::File, io::Read, path::PathBuf};
 
-#[derive(Parser)]
-struct Cli {
-    /// The CHIP-8 ROM file.
-    rom_file: PathBuf,
+#[derive(Subcommand)]
+enum Commands {
+    /// Assemble a CHIP-8 Assembly file.
+    Assemble {
+        /// Path to the CHIP-8 Assembly file.
+        asm_path: PathBuf,
 
-    /// Add ability to step through instructions individually.
-    #[arg(short, long)]
-    step: bool,
+        /// Path to output assembled code.
+        #[arg(short, long, default_value = "prog.c8out")]
+        outfile: PathBuf,
+    },
 
-    /// Prevent the display from being shown.
-    #[arg(long)]
-    no_display: bool,
+    /// Emulate a CHIP-8 program.
+    Emulate {
+        /// Path to the CHIP-8 program.
+        prog_path: PathBuf,
+
+        /// Step through instructions individually.
+        #[arg(short, long)]
+        step: bool,
+
+        /// Prevent the display from being shown.
+        #[arg(long)]
+        no_display: bool,
+    },
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let args = Cli::parse();
+#[derive(Parser)]
+#[command(author, about, version, propagate_version = true)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
 
+fn emulate(prog_path: PathBuf, step: bool, no_display: bool) -> Result<(), Box<dyn Error>> {
     // Setup needed sdl2 facilities.
     let sdl_context = sdl2::init()?;
     let window = sdl_context
@@ -45,15 +63,28 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mut cpu = Cpu::new(display, event_pump);
 
     // Read the program instructions into a buffer.
-    let mut rom = File::open(args.rom_file)?;
+    let mut rom = File::open(prog_path)?;
     let mut prog = Vec::new();
     rom.read_to_end(&mut prog)?;
 
     // Execute the program.
-    cpu.execute_program(&prog, args.step, args.no_display);
+    cpu.execute_program(&prog, step, no_display);
 
     // Dump post-execution state.
     cpu.dump_state();
 
     Ok(())
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args = Cli::parse();
+
+    match args.command {
+        Commands::Assemble { asm_path, outfile } => assembler::assemble(asm_path, outfile),
+        Commands::Emulate {
+            prog_path,
+            step,
+            no_display,
+        } => emulate(prog_path, step, no_display),
+    }
 }
